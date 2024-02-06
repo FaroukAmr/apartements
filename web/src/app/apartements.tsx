b2501ceb3a5a0ccc608209723d8b33e586c24833
FaroukAmr <faroukamr508@gmail.com>
2024-02-06 23:05:59 +0200

    Refactor ApartementsList component to use CSS modules

--- web/src/app/apartements.tsx
+++ web/src/app/apartements.tsx
@@ -1,7 +1,12 @@
 import { Apartement } from './models/Apartement';
+import styles from './page.module.css';
 
 async function getApartements() {
-  const response = await fetch('http://localhost:8080/apartements');
+  const response = await fetch('http://localhost:8080/apartements', {
+    next: {
+      revalidate: 0,
+    },
+  });
   return response.json();
 }
 
@@ -10,13 +15,17 @@ export default async function ApartementsList() {
   const apartements: Apartement[] = response.data;
   console.log(apartements);
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Apartements</h1>
-      <ul>
+      <div className={styles.apartementsContainer}>
         {apartements.map((apartement: any) => (
-          <li key={apartement.id}>{apartement.name}</li>
+          <div className={styles.apartement} key={apartement.id}>
+            <div>{apartement.name}</div>
+            <div>{apartement.description}</div>
+            <div>{apartement.price}</div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
