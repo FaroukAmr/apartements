9c18b0f898d781222f21161a4a83f461fbccc9ae
FaroukAmr <faroukamr508@gmail.com>
2024-02-07 19:25:21 +0200

    mobile init

---
+++ mobile/app/index.tsx
@@ -0,0 +1,15 @@
+import { COLORS, SIZES, icons, images } from '../constants';
+import { SafeAreaView, ScrollView, Text, View } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+
+import { useState } from 'react';
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
